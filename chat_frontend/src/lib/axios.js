c8f6063dd8021865b3d5dfd7b64fe367044e43b3
sanskarjain960 <sanskarjain960@gmail.com>
2025-04-15 16:46:24 +0530

    axios baseURL change

--- chat_frontend/src/lib/axios.js
+++ chat_frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: `${BASE_URL}/api`,
     withCredentials: true,
 })
