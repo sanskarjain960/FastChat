2b3e779d570cc024ce38deb2ab08d05aeec32352
sanskarjain960 <sanskarjain960@gmail.com>
2025-04-15 20:17:14 +0530

    jwt changes

--- chat_backend/src/utils/generateToken.js
+++ chat_backend/src/utils/generateToken.js
@@ -9,8 +9,8 @@ export const generateToken = (userId, res) =>{
     res.cookie("jwt",token, {
         maxAge: 7*24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: "development",
+        sameSite: "None",
+        secure: true,
     });
 
     return token;
