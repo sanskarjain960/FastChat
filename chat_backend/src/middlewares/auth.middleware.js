50e19c5575c42f33d31c823f696df389669245af
sanskarjain960 <sanskarjain960@gmail.com>
2025-04-15 20:10:36 +0530

    debug

--- chat_backend/src/middlewares/auth.middleware.js
+++ chat_backend/src/middlewares/auth.middleware.js
@@ -6,12 +6,12 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const protectRoute = asyncHandler(async (req, res,next) => {
         
     const token = req.cookies?.jwt;
-
-        console.log(token);
     
         if(!token){
             throw new ApiError(400,"Unauthorized - no token provided");
         } 
+
+        console.log(token);
     
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
     
